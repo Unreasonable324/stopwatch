a3961c9cd430136384139b23f7f3d97f2cd5a959
Unreasonable324 <a8surd324@yandex.ru>
2022-11-14 21:16:13 +0700

    ghj

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const time = document.querySelector('#time')
 const start = document.querySelector('#start')
 
-let timezero
+let timezero = 95
 
-start.addEventListener('click', (event) =>{
-    setInterval(decreaseTime, 1000)
-    setTime(timezero)
-}) 
+function startgame(){
+    setInterval(increaseTime, 1000)
+    setTime(timezero)}
+    
+
+start.addEventListener('click', function increaseTime() {
+    let current = ++timezero 
+        if (current < 10) {
+            current = `0${current}`
+        }else if (current > 100) {
+            current = `0${current}`
+        }
+        setTimeout(increaseTime, 1000)
+    
+        setTime(current)
+    })
+    
+
+function setTime(value){
+    time.innerHTML = `00:${value}`
+}
