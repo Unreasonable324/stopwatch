08ae55c8e06605c4e0874cfba512547180d9f318
Unreasonable324 <a8surd324@yandex.ru>
2022-11-16 20:31:40 +0700

    correct operation START and RESET

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let min = 0
 let hrs = 0
 let day = 0
 let year = 0
-let t
+let t = null;
 
 function increaseTime() {
     ++mlsec 
@@ -37,7 +37,8 @@ function increaseTime() {
 
 function display() {
     increaseTime()
-        time.textContent = (year > 9 ? year: '0' + year)
+        time.textContent = 
+        (year > 9 ? year: '0' + year)
         + ':' + 
         (day > 9 ? day: '0' + day)
         + ':' + 
@@ -48,43 +49,74 @@ function display() {
         (sec> 9 ? sec: '0' + sec)
         + '.' + 
         (mlsec> 9 ? mlsec: '0' + mlsec);
-            timer()
 } 
 
 function timer() {
-    t = setTimeout (display, 10)
+    if (t == null) {
+    t = setInterval (display, 10)
+    }
 }
 
-// function disabled(){
-//     timer.target.disabled = true
-// }
+function createScore() {
+    const score = document.createElement ('li')
+    score.classList.add('score')
+    score.id = ('aaa')
+    ol.append(score)
+    score.textContent = 
+    (year > 9 ? year: '0' + year)
+    + ':' + 
+    (day > 9 ? day: '0' + day)
+    + ':' + 
+    (hrs > 9 ? hrs: '0' + hrs)
+    + ':' + 
+    (min > 9 ? min: '0' + min)
+    + ':' + 
+    (sec> 9 ? sec: '0' + sec)
+    + '.' + 
+    (mlsec> 9 ? mlsec: '0' + mlsec);
+}
 
-// timer.target.disabled = true
+let ol = document.querySelector('#ol')
+// let li = document.querySelector('#aaa')
+function deleteLI (){
+// все три метода работают
+// 1    ol.innerHTML = ''
 
-start.addEventListener('click', () => timer())
-// start.addEventListener('click', timer => {
-//     timer.target.disabled = true}
-// )
-pause.addEventListener('click', () => {clearTimeout(t)} )
-reset.addEventListener('click', () => { clearTimeout(t)
-                                time.textContent = `00:00:00:00:00.00`
-                            mlsec = 0
-                        sec = 0
-                    min = 0
-                hrs = 0
-            day = 0
-        year = 0
-    } 
-)
-    // start.onclick = timer
-    // pause.onclick = () => { clearTimeout(t)}
-    // reset.onclick = () => {
-    //     clearTimeout(t)
-    //     time.textContent = `00:00:00:00:00:00`
-    //     mlsec = 0
-    //     sec = 0
-    //     min = 0
-    //     hrs = 0
-    //     day = 0
-    //     year = 0
-    // }
+/*2*/    while(ol.firstChild){
+        ol.removeChild(ol.lastChild)
+    }
+
+// 3    ol.querySelectorAll('*').forEach((n) => n.remove())
+}
+
+window.onload = () => {
+
+    start.addEventListener('click', () => {
+        
+        timer()})
+    pause.addEventListener('click', () => {
+        if(t != null) {
+            clearInterval(t)
+            t = null
+        }  
+        createScore()
+    })
+
+    reset.addEventListener('click', () => {
+        if(t != null) {
+            clearInterval(t)
+            t = null
+        }
+        time.textContent = `00:00:00:00:00.00`
+                                    mlsec = 0
+                                sec = 0
+                            min = 0
+                        hrs = 0
+                    day = 0
+                year = 0
+                deleteLI ()
+                
+            console.log(reset);}
+            
+        )
+}
